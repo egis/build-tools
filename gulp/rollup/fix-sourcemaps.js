d571d55b5c922f8aa2b26cddc8b60bf460e8af22
Artem Vasiliev <artem.job@gmail.com>
2015-12-08 14:15:23 +0300

    re EUI-15 (Rollup based build) sourcemapping hints

--- gulp/rollup/fix-sourcemaps.js
+++ gulp/rollup/fix-sourcemaps.js
@@ -3,6 +3,7 @@ var replace = require('gulp-replace');
 
 module.exports = function() {
     return gulp.src('dist/EgisUI.js.map')
-        .pipe(replace('../../src', ''))
+        //.pipe(replace('../../src', '../src')) //this loads correct sourcemaps for examples if used without uglifier
+        .pipe(replace('../../src', '')) //this makes uglifier not error about missing files, but it doesn't work still.
         .pipe(gulp.dest('dist/'));
 };
