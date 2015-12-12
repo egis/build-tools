0371b000ee4dfec3badeb3c073fe8b94a057512c
Artem Vasiliev <artem.job@gmail.com>
2015-12-12 17:16:13 +0300

    Revert "re EUI-15 (Rollup based build)"
    This reverts commit 601cc307e18e1735129779bbb697bfc95cf53c1c.

--- gulp/rollup/rollup.js
+++ gulp/rollup/rollup.js
@@ -15,7 +15,28 @@ module.exports = function(moduleName) {
             'EgisUI': 'EgisUI'
         },
         plugins: [ babel({
-            'presets': ['es2015-rollup'],
+            'plugins': [
+                [require("babel-plugin-transform-es2015-template-literals"), {loose: true}],
+                [require("babel-plugin-transform-es2015-literals"), {loose: true}],
+                [require("babel-plugin-transform-es2015-function-name"), {loose: true}],
+                [require("babel-plugin-transform-es2015-arrow-functions"), {loose: true}],
+                [require("babel-plugin-transform-es2015-block-scoped-functions"), {loose: true}],
+                [require("babel-plugin-transform-es2015-classes"), {loose: true}],
+                [require("babel-plugin-transform-es2015-object-super"), {loose: true}],
+                [require("babel-plugin-transform-es2015-shorthand-properties"), {loose: true}],
+                [require("babel-plugin-transform-es2015-computed-properties"), {loose: true}],
+                [require("babel-plugin-transform-es2015-for-of"), {loose: true}],
+                [require("babel-plugin-transform-es2015-sticky-regex"), {loose: true}],
+                [require("babel-plugin-transform-es2015-unicode-regex"), {loose: true}],
+                [require("babel-plugin-check-es2015-constants"), {loose: true}],
+                [require("babel-plugin-transform-es2015-spread"), {loose: true}],
+                [require("babel-plugin-transform-es2015-parameters"), {loose: true}],
+                [require("babel-plugin-transform-es2015-destructuring"), {loose: true}],
+                [require("babel-plugin-transform-es2015-block-scoping"), {loose: true}],
+                [require("babel-plugin-transform-es2015-typeof-symbol"), {loose: true}],
+                [require("babel-plugin-external-helpers-2"), {loose: true}],
+                [require("babel-plugin-transform-regenerator"), { async: false, asyncGenerators: false }]
+            ],
             'highlightCode': true
         }) ]
     })
