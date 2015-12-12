0eda7b54448e42faa952b640eae683f8ffdaafcc
Artem Vasiliev <artem.job@gmail.com>
2015-12-12 23:56:40 +0300

    re EUI-15 (Rollup based build) minor refactoring

--- gulp/dev-bundle.js
+++ gulp/dev-bundle.js
@@ -15,34 +15,30 @@ var connect = require('gulp-connect');
 
 require('./cleanup');
 
-function devCompilingPipeline(kind) {
-    var srcDir = common.srcDirs[kind];
-    var destDir = common.dist[kind];
-    var d0 = 0;
-    return gulp.src([srcDir + '/**/*.js', srcDir + '/.lib-exports.js', '!' + srcDir + '/**/*_scsslint_*'])
-        .pipe(changed(destDir))
-        .pipe(sourcemaps.init())
-        .once('data', function() {d0 = new Date().getTime()})
-        .pipe(plumber())
-        .pipe(debug())
-        .pipe(babel({
-            highlightCode: true,
-            presets: ['es2015']
-        }))
-        .pipe(print(function() {
-            var d = new Date().getTime();
-            var res = d - d0;
-            d0 = d;
-            return ['done in ', res, 'ms'].join("");
-        }))
-        .pipe(sourcemaps.write('.', {includeContent: true}))
-        .pipe(gulp.dest(destDir))
-        .pipe(connect.reload());
-}
-
 _.each(common.bundleKinds, function(kind) {
     gulp.task('dev-recompile-' + kind, [], function () {
-        return devCompilingPipeline(kind);
+        var srcDir = common.srcDirs[kind];
+        var destDir = common.dist[kind];
+        var d0 = 0;
+        return gulp.src([srcDir + '/**/*.js', srcDir + '/.lib-exports.js', '!' + srcDir + '/**/*_scsslint_*'])
+            .pipe(changed(destDir))
+            .pipe(sourcemaps.init())
+            .once('data', function() {d0 = new Date().getTime()})
+            .pipe(plumber())
+            .pipe(debug())
+            .pipe(babel({
+                highlightCode: true,
+                presets: ['es2015']
+            }))
+            .pipe(print(function() {
+                var d = new Date().getTime();
+                var res = d - d0;
+                d0 = d;
+                return ['done in ', res, 'ms'].join("");
+            }))
+            .pipe(sourcemaps.write('.', {includeContent: true}))
+            .pipe(gulp.dest(destDir))
+            .pipe(connect.reload());
     });
 
     gulp.task('generate-systemjs-' + kind + '-index', ['gen-stage2-wildcard-exports-' + kind, 'dev-recompile-' + kind], function() {
