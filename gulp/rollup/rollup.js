87d3f0690108fda0879b80de5143f52ffdedd43b
Artem Vasiliev <artem.job@gmail.com>
2015-12-15 00:04:13 +0300

    re EUI-15 (Rollup based build) allows importing own module for tests (just in case)

--- gulp/rollup/rollup.js
+++ gulp/rollup/rollup.js
@@ -9,6 +9,7 @@ var common = require('../common');
 module.exports = function(moduleName) {
     var globals = {};
     globals[common.egisUiModuleName] = common.egisUiModuleName;
+    globals[common.pkg.name] = common.pkg.name; //allows importing own module for tests
     return rollup({
         // any option supported by rollup can be set here, including sourceMap
         format: 'iife',
