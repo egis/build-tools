3ee0d070a6284ec2167b0238942b6fd915831174
Artem Vasiliev <artem.job@gmail.com>
2016-03-03 16:06:30 +0300

    docs: comment wording

--- gulp/rollup/compile-and-bundle.js
+++ gulp/rollup/compile-and-bundle.js
@@ -21,7 +21,7 @@ module.exports = function(bundleKind) {
     if (common.pkg.name !== common.egisUiPkgName) {
         // Make client apps' rollup build code run after the EgisUI.loaded: this is needed to make sure client
         // app can work with EgisUI in dev mode. This is because in dev mode app code is loaded asynchronously
-        // by SystemJS, and the code client app relies on can becomes available later.
+        // by SystemJS, and the code client app relies on may become available later.
         res = res.pipe(insert.wrap(common.egisUiModuleName + '.loaded(function() {', '});'));
     }
     return res
