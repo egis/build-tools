3fb88901a0caa93563050f7ef279678d570fe2d1
Artem Vasiliev <artem.job@gmail.com>
2016-10-03 19:52:07 +0300

    fix(dev build): dev mode for plugins
    * fast recompilation of individual JS files
    * JS maps work in dev mode
    * Livereload enabled (for multiple projects in dev mode first one wins)

--- gulp/plugin.js
+++ gulp/plugin.js
@@ -9,6 +9,8 @@ var exit = require('gulp-exit');
 var del = require('del');
 var sourcemaps = require('gulp-sourcemaps');
 var common = require('./common');
+var _ = require('lodash');
+var connect = require('gulp-connect');
 
 gulp.task('plugin_concat', ['compile-main', 'templates'], function() {
      return gulp.src([common.dist.main + "/**/*.js", common.dist.main + "/templates/*.js"])
@@ -19,25 +21,44 @@ gulp.task('plugin_concat', ['compile-main', 'templates'], function() {
         .pipe(gulp.dest('build/'))
 });
 
-gulp.task('plugin', ['plugin_concat'], function() {
+function packagePlugin() {
     var file = common.module.main + (common.pkg.plugin ? ".zip" : ".war");
     del.sync('tmp');
-    utils.sh("mkdir -p tmp/System/plugins/" + common.pkg.plugin + "/")
+    utils.sh("mkdir -p tmp/System/plugins/" + common.pkg.plugin + "/");
     utils.sh("mkdir -p tmp/PT-SCRIPTS");
-    utils.sh("rm build/*.js.gz || echo ok")
-    utils.sh("rm build/*.js.map.gz || echo ok" )
+    utils.sh("rm -rf build/System || echo ok");
+    utils.sh("rm build/*.js.gz || echo ok");
+    utils.sh("rm build/*.js.map.gz || echo ok" );
     if (!common.watch) {
         utils.sh("cp install.groovy tmp/PT-SCRIPTS/ || echo ok");
-        utils.sh("cp -R resources/* tmp/ || echo ok")
+        utils.sh("cp -R resources/* tmp/ || echo ok");
     }
-    utils.sh("cp -R build/* tmp/System/plugins/" + common.pkg.plugin + "/")
+    utils.sh("cp -R build/* tmp/System/plugins/" + common.pkg.plugin + "/");
     console.log('Deploying to ' + common.deploy + "/" + file);
     return gulp.src(["tmp/**/*"])
         .pipe(zip(file))
         .pipe(gulp.dest(common.deploy))
         .pipe(gulp.dest('.'))
-})
+}
 
-gulp.task("plugin_watch", ['plugin'], function() {
-    gulp.watch('src/**/*', ['plugin']);
-})
+gulp.task('plugin', ['plugin_concat'], function() {
+    return packagePlugin();
+});
+
+gulp.task('plugin-dev-package', ['dev-bundle-main', 'styles', 'resources'], function() {
+    return packagePlugin();
+});
+
+gulp.task('plugin-repackage', ['plugin-dev-package'], function() {
+    connect.reload();
+});
+
+gulp.task('plugin_watch', ['plugin-repackage', 'dev-bundle-tests', 'webserver'], function() {
+    _.each(common.bundleKinds, function(kind) {
+        gulp.watch([common.srcDirs[kind] + '/**/*.js'], ['dev-recompile-' + kind]);
+        gulp.watch([common.srcDirs[kind] + '/.lib-exports.js'], ['dev-recompile-' + kind, 'generate-systemjs-' + kind + '-index']);
+    });
+    gulp.watch(['**/.dev-loader.js'], ['plugin-repackage']);
+    gulp.watch('src/**/*.hbs', ['recompile-templates']);
+    gulp.watch('style/**/*.*', ['recompile-styles']);
+});
