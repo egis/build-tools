720a14bdeca536b54de0e4dba85f8d21a904a3fd
Artem Vasiliev <artem.job@gmail.com>
2017-07-31 17:05:28 +0300

    chore(build env): let's use Yarn
    .. for speed and reliability

---
+++ yarn.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+cp package.json package.json.bak
+cp yarn.lock yarn.lock.bak
+cp node_modules/@egis/build-tools/package.json .
+cp node_modules/@egis/build-tools/yarn.lock .
+yarn
+mv package.json.bak package.json
+mv yarn.lock.bak yarn.lock
