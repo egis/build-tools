3696af4450867a83554bd9cf551bf9b43c187143
Artem Vasiliev <artem.job@gmail.com>
2017-08-01 18:45:58 +0300

    chore(ci): let's not hardcode build-tools path

--- install-build-tools-deps.sh
+++ install-build-tools-deps.sh
@@ -2,7 +2,6 @@
 
 cp package.json package.json.bak
 BASEDIR=$(dirname "$0")
-echo "hello from install-build-tools-deps: $BASEDIR"
-node node_modules/@egis/build-tools/merge-build-tools-deps.js
+node $BASEDIR/merge-build-tools-deps.js
 yarn
 mv package.json.bak package.json
