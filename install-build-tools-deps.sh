ebd5434c9a708329831182aad326ec1bbf861d6c
Artem Vasiliev <artem.job@gmail.com>
2017-08-01 18:39:40 +0300

    chore(ci): debugging issue with Appveyor

--- install-build-tools-deps.sh
+++ install-build-tools-deps.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
-echo "hello from install-build-tools-deps"
 cp package.json package.json.bak
-npm run merge-build-tools-deps
+BASEDIR=$(dirname "$0")
+echo "hello from install-build-tools-deps: $BASEDIR"
+node node_modules/@egis/build-tools/merge-build-tools-deps.js
 yarn
 mv package.json.bak package.json
