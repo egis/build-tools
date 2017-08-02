edb075c3126bb3e7b86a95dec3e777a9a3d4284c
Artem Vasiliev <artem.job@gmail.com>
2017-08-02 14:25:00 +0300

    chore: let it be Node 4 - compatible
    node 4 has Object.assign but doesn't have "let"
    
    re switching to yarn

--- merge-build-tools-deps.js
+++ merge-build-tools-deps.js
@@ -1,11 +1,11 @@
 #!/usr/bin/env node
 
-const fs = require('fs');
+var fs = require('fs');
 
-let dst = fs.readFileSync('package.json');
-let dstJson = JSON.parse(dst);
-let srcPath = __dirname  + '/package.json';
-let src = fs.readFileSync(srcPath);
-let srcJson = JSON.parse(src);
+var dst = fs.readFileSync('package.json');
+var dstJson = JSON.parse(dst);
+var srcPath = __dirname  + '/package.json';
+var src = fs.readFileSync(srcPath);
+var srcJson = JSON.parse(src);
 dstJson.devDependencies = Object.assign(srcJson.devDependencies, dstJson.devDependencies);
 fs.writeFileSync('package.json', JSON.stringify(dstJson), 'utf8');
