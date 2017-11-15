1737e171b0996562037c4b4e844e3bc9a3f77902
Artem Vasiliev <artem.job@gmail.com>
2017-11-15 18:39:40 +0300

    refactor: move typescript gulp utilities to egis-ui
    .. where they are only needed currently
    
    re EUI-1124 (Generate and Publish typescript definitions)

--- gulp/typescript-defs.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var debug = require('gulp-debug');
-
-gulp.task('prepare-to-publish', function ()
-{
-    return gulp.src(['src/**/*.d.ts'])
-        .pipe(debug())
-        .pipe(gulp.dest('build'))
-});
