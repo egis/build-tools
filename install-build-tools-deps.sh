31cec87c0fe9fbf735ef6dabeb056eb95244b10f
Artem Vasiliev <artem.job@gmail.com>
2018-02-28 19:58:14 +0300

    chore(dont-break): use default `npm install` to install dep. project
    ..because globally installed yarn on CI cannot install private packages

--- install-build-tools-deps.sh
+++ install-build-tools-deps.sh
@@ -3,7 +3,7 @@
 cp package.json package.json.bak
 BASEDIR=$(dirname "$0")
 node $BASEDIR/merge-build-tools-deps.js
-yarn add yarn@1.5.1 && node_modules/.bin/yarn --ignore-engines
+node_modules/.bin/yarn --ignore-engines
 if [[ "$KEEP_MODIFIED_PACKAGE" != 'true' ]]; then
     mv package.json.bak package.json
 fi
--- install-dont-break.sh
+++ install-dont-break.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node_modules/.bin/yarn global add --ignore-engines dont-break@1.12.1
+node_modules/.bin/yarn global add --ignore-engines @artemv/dont-break@1.12.2-pre.1
