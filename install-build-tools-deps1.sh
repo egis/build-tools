ea3439999ab9368ec9b4e24694baa49ec3b2bed8
Artem Vasiliev <artem.job@gmail.com>
2018-03-01 00:27:26 +0300

    chore: removed extra script

--- install-build-tools-deps1.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-BASEDIR=$(dirname "$0")
-YARN_LOCATION=node_modules/$CURRENT_MODULE_NAME/node_modules/.bin $BASEDIR/install-build-tools-deps.sh
--- update-build-tools-deps.sh
+++ update-build-tools-deps.sh
@@ -3,7 +3,10 @@
 cp package.json package.json.bak
 BASEDIR=$(dirname "$0")
 node $BASEDIR/merge-build-tools-deps.js
-node_modules/.bin/yarn --ignore-engines
+if [[ "$YARN_LOCATION" == "" ]]; then
+    YARN_LOCATION=node_modules/.bin
+fi
+$YARN_LOCATION/yarn --ignore-engines
 rc=$?
 if [[ "$KEEP_MODIFIED_PACKAGE" != 'true' ]]; then
     mv package.json.bak package.json
