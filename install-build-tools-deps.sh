ff877ff8f1d18df6a9c0a13e30df25bf7749f4f2
Artem Vasiliev <artem.job@gmail.com>
2018-03-01 00:16:00 +0300

    chore(dont-break): corrected re yarn

--- install-build-tools-deps.sh
+++ install-build-tools-deps.sh
@@ -3,9 +3,8 @@
 cp package.json package.json.bak
 BASEDIR=$(dirname "$0")
 node $BASEDIR/merge-build-tools-deps.js
-if [[ "$SKIP_YARN_INSTALL" != 'true' ]]; then
-    npm install yarn@1.5.1
-    $YARN_LOCATION=node_modules/.bin
+if [[ "$YARN_LOCATION" == "" ]]; then
+    YARN_LOCATION=node_modules/.bin
 fi
 $YARN_LOCATION/yarn --ignore-engines
 if [[ "$KEEP_MODIFIED_PACKAGE" != 'true' ]]; then
--- install-build-tools-deps1.sh
+++ install-build-tools-deps1.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 BASEDIR=$(dirname "$0")
-SKIP_YARN_INSTALL=true YARN_LOCATION=node_modules/$CURRENT_MODULE_NAME/node_modules/.bin $BASEDIR/install-build-tools-deps.sh
+YARN_LOCATION=node_modules/$CURRENT_MODULE_NAME/node_modules/.bin $BASEDIR/install-build-tools-deps.sh
