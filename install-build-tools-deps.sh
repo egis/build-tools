82ce09f5b7e28f722d6833da77fe27549b6e92b4
Artem Vasiliev <artem.job@gmail.com>
2018-02-28 12:39:49 +0300

    chore(dont-break): use local yarn
    re EUI-1249 (Switch EgisUI and related projects to Node 8 at CI for better build stability)

--- install-build-tools-deps.sh
+++ install-build-tools-deps.sh
@@ -3,7 +3,7 @@
 cp package.json package.json.bak
 BASEDIR=$(dirname "$0")
 node $BASEDIR/merge-build-tools-deps.js
-npm install yarn && yarn --ignore-engines
+npm install yarn && node_modules/.bin/yarn --ignore-engines
 if [[ "$KEEP_MODIFIED_PACKAGE" != 'true' ]]; then
     mv package.json.bak package.json
 fi
