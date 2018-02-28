272d5944aa8eac970cb6ce610c38342a4b035a98
Artem Vasiliev <artem.job@gmail.com>
2018-02-28 20:53:11 +0300

    chore(dont-break): corrected to make sure correct yarn version is used
    ..when setting up dependent project

--- install-build-tools-deps.sh
+++ install-build-tools-deps.sh
@@ -3,7 +3,7 @@
 cp package.json package.json.bak
 BASEDIR=$(dirname "$0")
 node $BASEDIR/merge-build-tools-deps.js
-node_modules/.bin/yarn --ignore-engines
+node_modules/@egis/build-tools/node_modules/.bin/yarn --ignore-engines
 if [[ "$KEEP_MODIFIED_PACKAGE" != 'true' ]]; then
     mv package.json.bak package.json
 fi
--- install-dont-break.sh
+++ install-dont-break.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node_modules/.bin/yarn global add --ignore-engines @artemv/dont-break@1.12.2-pre.3
+node_modules/.bin/yarn global add --ignore-engines @artemv/dont-break@1.12.2-pre.4
