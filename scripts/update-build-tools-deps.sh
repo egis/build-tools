8d3e2da6526f839354b83c24fa553ec083ba0979
Artem Vasiliev <artem.job@gmail.com>
2018-03-02 14:09:43 +0300

    fix(ci): node 4.2.6 compatibility for `npm run update`

--- scripts/update-build-tools-deps.sh
+++ scripts/update-build-tools-deps.sh
@@ -4,7 +4,8 @@ cp package.json package.json.bak
 BASEDIR=$(dirname "$0")
 node $BASEDIR/merge-build-tools-deps.js
 if [[ "$YARN_LOCATION" == "" ]]; then
-    YARN_LOCATION=node_modules/.bin
+    test -f node_modules/.bin/yarn && YARN_LOCATION=node_modules/.bin
+    test -f node_modules/@egis/build-tools/node_modules/.bin/yarn && YARN_LOCATION=node_modules/@egis/build-tools/node_modules/.bin
 fi
 $YARN_LOCATION/yarn --ignore-engines
 rc=$?
