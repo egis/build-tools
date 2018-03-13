e3d125c150205b047286011f5517cda81efa0969
Artem Vasiliev <artem.job@gmail.com>
2018-03-13 17:23:33 +0300

    chore: don't install local yarn before `npm run setup`
    .. otherwise it seems to change package.json cancelling caching.
    
    And then use global yarn here and there.
    
    re EUI-1258 (Switch egis-ui related repos to circle2.0)

--- scripts/install-dont-break.sh
+++ scripts/install-dont-break.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node_modules/.bin/yarn global add --ignore-engines dont-break@1.12.2
+yarn global add --ignore-engines dont-break@1.12.2
--- scripts/install-npm-retry.sh
+++ scripts/install-npm-retry.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-node_modules/.bin/yarn global add --ignore-engines npm-install-retry@2.0.0
+yarn global add --ignore-engines npm-install-retry@2.0.0
