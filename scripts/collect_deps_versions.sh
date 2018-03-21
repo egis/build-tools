5b686dad6061df07771591150f72d4e72e3cec4c
Artem Vasiliev <artem.job@gmail.com>
2018-03-22 01:17:51 +0300

    chore(ci): minor
    re EUI-1259 (build-tools: optimize re `npm run update`)

--- scripts/collect_deps_versions.sh
+++ scripts/collect_deps_versions.sh
@@ -3,4 +3,4 @@
 grep "version\"\:" node_modules/*/*/package.json > $OUT_DIR/npm_versions.txt
 grep "version\"\:" node_modules/*/package.json >> $OUT_DIR/npm_versions.txt
 grep "version\"\:" node_modules/*/*/*/package.json >> $OUT_DIR/npm_versions.txt
-([ -d bower_components ] && grep "version\"\:" bower_components/*/.bower.json > $OUT_DIR/bower_versions.txt) || echo no bower
+(grep "version\"\:" bower_components/*/.bower.json > $OUT_DIR/bower_versions.txt) || echo no bower
