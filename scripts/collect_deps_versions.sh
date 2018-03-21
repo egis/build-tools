5860746b61a82611a3b9b2a53d2b061c3b64d7d9
Artem Vasiliev <artem.job@gmail.com>
2018-03-22 00:09:26 +0300

    chore(ci): another CI helper
    re EUI-1259 (build-tools: optimize re `npm run update`)

---
+++ scripts/collect_deps_versions.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+grep "version\"\:" node_modules/*/*/package.json >> $OUT_DIR/npm_versions.txt
+grep "version\"\:" node_modules/*/package.json >> $OUT_DIR/npm_versions.txt
+grep "version\"\:" node_modules/*/*/*/package.json >> $OUT_DIR/npm_versions.txt
+[ -d bower_components ] && grep "version\"\:" bower_components/*/.bower.json > $OUT_DIR/bower_versions.txt
