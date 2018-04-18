c7cec008f3d854426126353d89c7921fe4c011b4
Artem Vasiliev <artem.job@gmail.com>
2018-04-18 20:24:49 +0300

    chore(ci): corrected re circle2.0
    re PPT-11509 (ProjectBootsrap - Strap Ci2 Compatible Projects)

--- scripts/ci/install-gradle.sh
+++ scripts/ci/install-gradle.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-if [ ! -e "/home/ubuntu/gradle.zip" ]; then  wget -O /home/ubuntu/gradle.zip https://services.gradle.org/distributions/gradle-2.7-all.zip; fi
-if [ ! -e "gradle-2.7" ] ; then unzip /home/ubuntu/gradle.zip; fi
+if [ ! -e "~/gradle.zip" ]; then wget -O ~/gradle.zip https://services.gradle.org/distributions/gradle-2.7-all.zip; fi
+if [ ! -e "gradle-2.7" ] ; then unzip ~/gradle.zip; fi
