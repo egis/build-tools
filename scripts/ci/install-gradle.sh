02fa9f254662d350585df156226d8d1bac4365cb
Artem Vasiliev <artem.job@gmail.com>
2018-04-18 20:46:01 +0300

    chore(ci): set TERM env var for gradle
    re PPT-11509 (ProjectBootsrap - Strap Ci2 Compatible Projects)

--- scripts/ci/install-gradle.sh
+++ scripts/ci/install-gradle.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
 
+echo 'export TERM=xterm' >> $BASH_ENV
 if [ ! -e "~/gradle.zip" ]; then wget -O ~/gradle.zip https://services.gradle.org/distributions/gradle-2.7-all.zip; fi
 if [ ! -e "gradle-2.7" ] ; then unzip ~/gradle.zip; fi
