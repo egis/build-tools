233aa23958f2d669968830d57003cf6ba9c23227
Artem Vasiliev <artem.job@gmail.com>
2018-08-02 09:17:03 +0300

    chore(style): minor refactoring as per Codacy advice

--- scripts/semantic-release.sh
+++ scripts/semantic-release.sh
@@ -2,7 +2,7 @@
 
 if [ "${CIRCLE_PROJECT_USERNAME}" == "egis" ] && [ "${CIRCLE_BRANCH}" == "master" ]; then
     node_modules/.bin/semantic-release pre || true
-    ((cat package.json | jq '.version' | grep -v semantic) && npm publish) || true
+    ((jq '.version' package.json | grep -v semantic) && npm publish) || true
     node_modules/.bin/semantic-release post || true
     if [ "${SEMANTIC_DEPENDENTS_UPDATES}" == "true" ]; then
         node_modules/.bin/semantic-dependents-updates-github || true
