7f3a764aa06c4c2b6ee6c702d4d62e7863c08e4c
Artem Vasiliev <artem.job@gmail.com>
2018-08-15 16:36:24 +0300

    chore(partials): same in other words
    previous version stopped working after lodash 3->4 upgrade

--- gulp/partials.js
+++ gulp/partials.js
@@ -15,16 +15,15 @@ module.exports = function ()
     return gulp.src(['src/**/_*.hbs'])
         .pipe(plumber())
         .pipe(handlebars())
-        .pipe(wrap('Handlebars.registerPartial(<%= processPartialName(file.relative) %>, Handlebars.template(<%= contents %>));', {}, {
-            imports: {
-                processPartialName: function (fileName)
-                {
-                    // Strip the extension and the underscore
-                    // Escape the output with JSON.stringify
-                    return JSON.stringify(path.basename(fileName, '.js').substr(1));
-                }
-            }
-        }))
+        .pipe(wrap('Handlebars.registerPartial(<%= partialName %>, Handlebars.template(<%= contents %>));',
+            function (file) {
+                let fileName = file.relative;
+                // Strip the extension and the underscore
+                // Escape the output with JSON.stringify
+                return {
+                    partialName: JSON.stringify(path.basename(fileName, '.js').substr(1))
+                };
+            }))
         .pipe(concat('partials.js'))
         .pipe(gulp.dest(common.dist.main + '/templates'));
 };
