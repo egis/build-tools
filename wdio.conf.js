c4031604284b2a34758c96f491b7ff0135e1df9b
Alexandr Pichkur <zloyag@zloyag-mac.local>
2018-09-27 17:13:30 +0300

    bug: EUI-1393 Summary tab does not refresh like the HIstory after the document was signed
    Added webdriverajax plugin
    https://github.com/chmanie/webdriverajax

--- wdio.conf.js
+++ wdio.conf.js
@@ -126,7 +126,9 @@ module.exports = {
     //     webdriverrtc: {},
     //     browserevent: {}
     // },
-    //
+    plugins: {
+        webdriverajax: {}
+    },
     // Test runner services
     // Services take over a specific job you don't want to take care of. They enhance
     // your test setup with almost no effort. Unlike plugins, they don't add new
