0fec7011195542cd0006fc15d5569b8fb9072cf7
Artem Vasiliev <artem.job@gmail.com>
2019-03-11 23:05:34 +0300

    chore(ci): moved couple scripts to simple-semantic-config
    BREAKING CHANGE: renamed them also

--- scripts/semantic-release.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-if [ "${CIRCLE_PROJECT_USERNAME}" == "egis" ] && [ "${CIRCLE_BRANCH}" == "master" ]; then
-    yarn semantic-release || true
-    if [ "${SEMANTIC_DEPENDENTS_UPDATES}" == "true" ]; then
-        node_modules/.bin/semantic-dependents-updates-github || true
-    fi
-fi
