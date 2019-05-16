5b346e2f0f65cf6f9897117af3ebee696e08bb20
Artem Vasiliev <artem.job@gmail.com>
2019-05-16 12:24:48 +0300

    chore(ci): minor

--- scripts/dont-break-tests.sh
+++ scripts/dont-break-tests.sh
@@ -1,2 +1,2 @@
 # note that this is executed from client project folder, after `npm install`
-yarn dependencies && yarn build && yarn test:build && yarn test -- --browsers=Chrome --reporters=junit,html,verbose
+yarn dependencies && yarn build && yarn test:build && yarn test --browsers=Chrome --reporters=junit,html,verbose
