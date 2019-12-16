c449f38c36bc6fd8bc4c93d4247096fe1ca59b8f
Artem Vasiliev <artem.job@gmail.com>
2019-12-16 20:57:46 +0300

    fix: corrected re partials compiling
    gulp-wrap works a bit differently now: the data function is called only once

--- gulp/partials.js
+++ gulp/partials.js
@@ -7,7 +7,6 @@ var plumber = require('gulp-plumber');
 var concat = require('gulp-concat');
 var handlebars = require('gulp-handlebars');
 var wrap = require('gulp-wrap');
-var path = require('path');
 var common = require('./common');
 
 module.exports = function ()
@@ -15,15 +14,7 @@ module.exports = function ()
     return gulp.src(['src/**/_*.hbs'])
         .pipe(plumber())
         .pipe(handlebars())
-        .pipe(wrap('Handlebars.registerPartial(<%= partialName %>, Handlebars.template(<%= contents %>));',
-            function (file) {
-                let fileName = file.relative;
-                // Strip the extension and the underscore
-                // Escape the output with JSON.stringify
-                return {
-                    partialName: JSON.stringify(path.basename(fileName, '.js').substr(1))
-                };
-            }))
+        .pipe(wrap('Handlebars.registerPartial("<%= file.stem.substr(1) %>", Handlebars.template(<%= contents %>));'))
         .pipe(concat('partials.js'))
         .pipe(gulp.dest(common.dist.main + '/templates'));
 };
